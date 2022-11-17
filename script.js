8bff25f347fbcefefa7299e719cb109a2542e6a4
Calvin Chern <calvin.chern24@gmail.com>
2022-11-17 17:11:25 -0500

    another url change

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ var questions = [
           everyscore = JSON.parse(everyscore);
         }
         everyscore.push(finalScore);
-        //sorting the scores to make them in order of ranking
+        //sorting the scores to make them in order of rankings
         everyscore.sort((a,b) => {return b.score-a.score});
 
 
